37f5908b3f87cf0c982d8e1cf1b5d52e0845979b
Nima Shoghi <nimashoghi@gmail.com>
2019-02-27 12:10:20 -0500

    Added new functions to the Validation module Added unit tests for new functions

--- src/Validation/Library.fs
+++ src/Validation/Library.fs
@@ -1,51 +1,149 @@
-﻿module Validation
+﻿namespace global
 
 type Validation<'value, 'error> = Result<'value, 'error list>
 
-/// **Description**
-///     Applies the function `f` to the value inside container `x`.
-let map (f: _ -> _) (x: Validation<_, _>): Validation<_, _> = Result.map f x
-
-/// **Description**
-///     Applies the function `f` to the error inside container `x`.
-let mapError (f: _ -> _) (x: Validation<_, _>): Validation<_, _> = Result.mapError f x
-
-/// **Description**
-///     Applies the function `f` to the error inside container `x` and flattens the result.
-let bind (f: _ -> Validation<_, _>) (x: Validation<_, _>): Validation<_, _> = Result.bind f x
-
-/// **Description**
-///     Applies the function inside the container `f` to the value inside the container `x`.
-let apply (f: Validation<_ -> _, _>) (x: Validation<_, _>): Validation<_, _> =
-    match f, x with
-    | Ok f, Ok x -> Ok (f x)
-    | Error lhs, Error rhs -> Error (lhs @ rhs)
-    | Error lhs, _ -> Error lhs
-    | _, Error rhs -> Error rhs
-
-module Operators =
+module Validation =
     /// **Description**
-    ///     Validation map operator
-    let (<!>) f x = map f x
+    ///     Applies the function `f` to the value inside container `x`.
+    let map (f: _ -> _) (x: Validation<_, _>): Validation<_, _> = Result.map f x
 
     /// **Description**
-    ///     Validation apply operator
-    let (<*>) f x = apply f x
+    ///     Applies the function `f` to the error inside container `x`.
+    let mapError (f: _ -> _) (x: Validation<_, _>): Validation<_, _> = Result.mapError f x
 
     /// **Description**
-    ///     Validation bind operator
-    let (>>=) x f = bind f x
+    ///     Applies the function `f` to the error inside container `x` and flattens the result.
+    let bind (f: _ -> Validation<_, _>) (x: Validation<_, _>): Validation<_, _> = Result.bind f x
 
-module Builder =
-    open Operators
+    /// **Description**
+    ///     Applies the function inside the container `f` to the value inside the container `x`.
+    let apply (f: Validation<_ -> _, _>) (x: Validation<_, _>): Validation<_, _> =
+        match f, x with
+        | Ok f, Ok x -> Ok (f x)
+        | Error lhs, Error rhs -> Error (lhs @ rhs)
+        | Error lhs, _ -> Error lhs
+        | _, Error rhs -> Error rhs
 
-    type ValidationBuilder<'error> () =
-        member __.Yield (_: unit): Validation<_, 'error> = Ok (unbox<_> null)
-        member __.For (x: Validation<'value, 'error>, f: 'value -> Validation<_, 'error>) = bind f x
-        [<CustomOperation ("validate", IsLikeZip = true)>]
-        member __.Validate (outer: Validation<'outer, 'error>, inner: Validation<'inner, 'error>, f: 'outer -> 'inner -> 'result): Validation<'result, 'error> =
-            f <!> outer <*> inner
-        member __.Return (x: 'value): Validation<'value, 'error> = Ok x
-        member __.ReturnFrom (x:  Validation<_, 'error>) = x
+    /// **Description**
+    ///     Converts `x` to an option.
+    let toOption (x: Validation<'value, 'error>): 'value option =
+        match x with
+        | Ok value -> Some value
+        | _ -> None
 
-    let validation<'error> = ValidationBuilder<'error> ()
+    /// **Description**
+    ///     Converts `x` to a value option.
+    let toValueOption (x: Validation<'value, 'error>): 'value voption =
+        match x with
+        | Ok value -> ValueSome value
+        | _ -> ValueNone
+
+    /// **Description**
+    ///     Converts `x` from a `'value option` to a `Validation<'value, 'error>`, returning the result of `error ()` if `x` is `None`.
+    let ofOptionWith (error: unit -> 'error list) (x: 'value option): Validation<'value, 'error> =
+        match x with
+        | Some value -> Ok value
+        | None -> Error (error ())
+
+    /// **Description**
+    ///     Converts `x` from a `'value option` to a `Validation<'value, 'error>`, returning `error` if `x` is `None`.
+    let ofOption (error: 'error list) (x: 'value option): Validation<'value, 'error> =
+        ofOptionWith (fun () -> error) x
+
+    /// **Description**
+    ///     Converts `x` from a `'value voption` to a `Validation<'value, 'error>`, returning the result of `error ()` if `x` is `ValueNone`.
+    let ofValueOptionWith (error: unit -> 'error list) (x: 'value voption): Validation<'value, 'error> =
+        match x with
+        | ValueSome value -> Ok value
+        | ValueNone -> Error (error ())
+
+    /// **Description**
+    ///     Converts `x` from a `'value voption` to a `Validation<'value, 'error>`, returning `error` if `x` is `ValueNone`.
+    let ofValueOption (error: 'error list) (x: 'value voption): Validation<'value, 'error> =
+        ofValueOptionWith (fun () -> error) x
+
+    /// **Description**
+    ///     Calls `f` on the value inside of `x`. If the result is `Some`, it returns that value. Otherwise, returns the result of `error ()`.
+    let tryPickWith (error: unit -> 'error list) (f: 'value -> 'result option) (x: Validation<'value, 'error>): Validation<'result, 'error> =
+        bind (f >> ofOptionWith error) x
+
+    /// **Description**
+    ///     Calls `f` on the value inside of `x`. If the result is `Some`, it returns that value. Otherwise, returns `error`.
+    let tryPick (error: 'error list) (f: 'value -> 'result option) (x: Validation<'value, 'error>): Validation<'result, 'error> =
+        tryPickWith (fun () -> error) f x
+
+    /// **Description**
+    ///     Calls `f` on the value inside of `x`. If the result is `true`, it returns that value. Otherwise, returns `error`.
+    let tryFind (error: 'error list) (f: 'value -> bool) (x: Validation<'value, 'error>): Validation<'value, 'error> =
+        tryPickWith (fun () -> error) (fun value -> if f value then Some value else None) x
+
+    /// **Description**
+    ///     Returns `true` if the value inside of `x` is equal to `value`.
+    let contains (value: 'value) (x: Validation<'value, 'error>): bool =
+        match x with
+        | Ok value' when value' = value -> true
+        | _ -> false
+
+    /// **Description**
+    ///     Flattens the nested validation object `x` of type `Validation<Validation<'value, 'error>>` into a `Validation<'value, 'error>`
+    let flatten (x: Validation<Validation<'value, 'error>, 'error>): Validation<'value, 'error> = bind id x
+
+    /// **Description**
+    ///     If `x` is `Error []`, returns the result of calling `f`. Otherwise, it forwards `x`.
+    let defaultWith (f: unit -> Validation<'value, 'error>) (x: Validation<'value, 'error>): Validation<'value, 'error> =
+        match x with
+        | Ok value -> Ok value
+        | Error [] -> f ()
+        | Error err -> Error err
+
+    /// **Description**
+    ///     If `x` is `Error []`, returns `defaultValue`. Otherwise, it forwards `x`.
+    let defaultValue (defaultValue: Validation<'value, 'error>) (x: Validation<'value, 'error>): Validation<'value, 'error> =
+        defaultWith (fun () -> defaultValue) x
+
+    /// **Description**
+    ///     If `x` is `Error []`, returns the result of calling `f >> Ok`. Otherwise, it forwards `x`.
+    let defaultSuccessWith (f: unit -> 'value) (x: Validation<'value, 'error>): Validation<'value, 'error> =
+        defaultWith (f >> Ok) x
+
+    /// **Description**
+    ///     If `x` is `Error []`, returns `Ok defaultValue`. Otherwise, it forwards `x`.
+    let defaultSuccess (defaultValue: 'value) (x: Validation<'value, 'error>): Validation<'value, 'error> =
+        defaultSuccessWith (fun () -> defaultValue) x
+
+    /// **Description**
+    ///     If `x` is `Error []`, returns the result of calling `f >> Error`. Otherwise, it forwards `x`.
+    let defaultErrorWith (f: unit -> 'error list) (x: Validation<'value, 'error>): Validation<'value, 'error> =
+        defaultWith (f >> Error) x
+
+    /// **Description**
+    ///     If `x` is `Error []`, returns `Error defaultValue`. Otherwise, it forwards `x`.
+    let defaultError (defaultValue: 'error list) (x: Validation<'value, 'error>): Validation<'value, 'error> =
+        defaultErrorWith (fun () -> defaultValue) x
+
+    module Operators =
+        /// **Description**
+        ///     Validation map operator
+        let (<!>) f x = map f x
+
+        /// **Description**
+        ///     Validation apply operator
+        let (<*>) f x = apply f x
+
+        /// **Description**
+        ///     Validation bind operator
+        let (>>=) x f = bind f x
+
+    module Builder =
+        open Operators
+
+        type ValidationBuilder<'error> () =
+            member __.Yield (_: unit): Validation<_, 'error> = Ok (unbox<_> null)
+            member __.For (x: Validation<'value, 'error>, f: 'value -> Validation<_, 'error>) = bind f x
+            [<CustomOperation ("validate", IsLikeZip = true)>]
+            member __.Validate (outer: Validation<'outer, 'error>, inner: Validation<'inner, 'error>, f: 'outer -> 'inner -> 'result): Validation<'result, 'error> =
+                f <!> outer <*> inner
+            member __.Return (x: 'value): Validation<'value, 'error> = Ok x
+            member __.ReturnFrom (x:  Validation<_, 'error>) = x
+
+        let validation<'error> = ValidationBuilder<'error> ()
--- tests/Validation.UnitTests/Validation.fs
+++ tests/Validation.UnitTests/Validation.fs
@@ -29,3 +29,182 @@ let ``bind success`` () = Validation.bind (fun a -> Ok (a + 2)) (Ok 1) =! Ok 3
 
 [<Test>]
 let ``bind failure`` () = Validation.bind (fun _ -> Error ["myError"]) (Ok 1) =! Error ["myError"]
+
+[<Test>]
+let ``toOption success`` () = Validation.toOption (Ok 1) =! Some 1
+
+[<Test>]
+let ``toOption failure`` () = Validation.toOption (Error ["myError"]) =! None
+
+[<Test>]
+let ``toValueOption success`` () = Validation.toValueOption (Ok 1) =! ValueSome 1
+
+[<Test>]
+let ``toValueOption failure`` () = Validation.toValueOption (Error ["myError"]) =! ValueNone
+
+[<Test>]
+let ``ofOptionWith success`` () = Validation.ofOptionWith (fun () -> ["myError"]) (Some 1) =! Ok 1
+
+[<Test>]
+let ``ofOptionWith failure`` () = Validation.ofOptionWith (fun () -> ["myError"]) None =! Error ["myError"]
+
+[<Test>]
+let ``ofValueOptionWith success`` () = Validation.ofValueOptionWith (fun () -> ["myError"]) (ValueSome 1) =! Ok 1
+
+[<Test>]
+let ``ofValueOptionWith failure`` () = Validation.ofValueOptionWith (fun () -> ["myError"]) ValueNone =! Error ["myError"]
+
+[<Test>]
+let ``ofOption success`` () = Validation.ofOption ["myError"] (Some 1) =! Ok 1
+
+[<Test>]
+let ``ofOption failure`` () = Validation.ofOption ["myError"] None =! Error ["myError"]
+
+[<Test>]
+let ``ofValueOption success`` () = Validation.ofValueOption ["myError"] (ValueSome 1) =! Ok 1
+
+[<Test>]
+let ``ofValueOption failure`` () = Validation.ofValueOption ["myError"] ValueNone =! Error ["myError"]
+
+[<Test>]
+let ``tryPick ok success`` () =
+    Validation.tryPick
+        ["myError"]
+        (fun x -> if x = 1 then Some "hello" else None)
+        (Ok 1)
+    =! Ok "hello"
+
+[<Test>]
+let ``tryPick ok failure`` () =
+    Validation.tryPick
+        ["myError"]
+        (fun x -> if x = 1 then Some "hello" else None)
+        (Ok 2)
+    =! Error ["myError"]
+
+[<Test>]
+let ``tryPick error success`` () =
+    Validation.tryPick
+        ["myError"]
+        (fun x -> if x = 1 then Some "hello" else None)
+        (Error ["myOtherError"])
+    =! Error ["myOtherError"]
+
+[<Test>]
+let ``tryPick error failure`` () =
+    Validation.tryPick
+        ["myError"]
+        (fun x -> if x = 1 then Some "hello" else None)
+        (Error ["myOtherError"])
+    =! Error ["myOtherError"]
+
+
+[<Test>]
+let ``tryFind ok success`` () =
+    Validation.tryFind
+        ["myError"]
+        (fun x -> x = 1)
+        (Ok 1)
+    =! Ok 1
+
+[<Test>]
+let ``tryFind ok failure`` () =
+    Validation.tryFind
+        ["myError"]
+        (fun x -> x = 1)
+        (Ok 2)
+    =! Error ["myError"]
+
+[<Test>]
+let ``tryFind error success`` () =
+    Validation.tryFind
+        ["myError"]
+        (fun x -> x = 1)
+        (Error ["myOtherError"])
+    =! Error ["myOtherError"]
+
+[<Test>]
+let ``tryFind error failure`` () =
+    Validation.tryFind
+        ["myError"]
+        (fun x -> x = 1)
+        (Error ["myOtherError"])
+    =! Error ["myOtherError"]
+
+[<Test>]
+let ``contains success`` () = Validation.contains 1 (Ok 1) =! true
+
+[<Test>]
+let ``contains failure`` () = Validation.contains 1 (Error ["myError"]) =! false
+
+[<Test>]
+let ``defaultValue default ok underlying ok`` () = Validation.defaultValue (Ok 1) (Ok 2) =! Ok 2
+
+[<Test>]
+let ``defaultValue default ok underlying empty`` () = Validation.defaultValue (Ok 1) (Error []) =! Ok 1
+
+[<Test>]
+let ``defaultValue default ok underlying error`` () = Validation.defaultValue (Ok 1) (Error ["myError"]) =! Error ["myError"]
+
+[<Test>]
+let ``defaultValue default error underlying ok`` () = Validation.defaultValue (Error ["myError2"]) (Ok 2) =! Ok 2
+
+[<Test>]
+let ``defaultValue default error underlying empty`` () = Validation.defaultValue (Error ["myError2"]) (Error []) =! Error ["myError2"]
+
+[<Test>]
+let ``defaultValue default error underlying error`` () = Validation.defaultValue (Error ["myError2"]) (Error ["myError"]) =! Error ["myError"]
+
+[<Test>]
+let ``defaultWith default ok underlying ok`` () = Validation.defaultWith (fun () -> Ok 1) (Ok 2) =! Ok 2
+
+[<Test>]
+let ``defaultWith default ok underlying empty`` () = Validation.defaultWith (fun () -> Ok 1) (Error []) =! Ok 1
+
+[<Test>]
+let ``defaultWith default ok underlying error`` () = Validation.defaultWith (fun () -> Ok 1) (Error ["myError"]) =! Error ["myError"]
+
+[<Test>]
+let ``defaultWith default error underlying ok`` () = Validation.defaultWith (fun () -> Error ["myError2"]) (Ok 2) =! Ok 2
+
+[<Test>]
+let ``defaultWith default error underlying empty`` () = Validation.defaultWith (fun () -> Error ["myError2"]) (Error []) =! Error ["myError2"]
+
+[<Test>]
+let ``defaultWith default error underlying error`` () = Validation.defaultWith (fun () -> Error ["myError2"]) (Error ["myError"]) =! Error ["myError"]
+
+[<Test>]
+let ``defaultSuccess underlying ok`` () = Validation.defaultSuccess 1 (Ok 2) =! Ok 2
+
+[<Test>]
+let ``defaultSuccess underlying empty`` () = Validation.defaultSuccess 1 (Error []) =! Ok 1
+
+[<Test>]
+let ``defaultSuccess underlying error`` () = Validation.defaultSuccess 1 (Error ["myError"]) =! Error ["myError"]
+
+[<Test>]
+let ``defaultError underlying ok`` () = Validation.defaultError ["myError2"] (Ok 2) =! Ok 2
+
+[<Test>]
+let ``defaultError underlying empty`` () = Validation.defaultError ["myError2"] (Error []) =! Error ["myError2"]
+
+[<Test>]
+let ``defaultError underlying error`` () = Validation.defaultError ["myError2"] (Error ["myError"]) =! Error ["myError"]
+
+[<Test>]
+let ``defaultSuccessWith underlying ok`` () = Validation.defaultSuccessWith (fun () -> 1) (Ok 2) =! Ok 2
+
+[<Test>]
+let ``defaultSuccessWith underlying empty`` () = Validation.defaultSuccessWith (fun () -> 1) (Error []) =! Ok 1
+
+[<Test>]
+let ``defaultSuccessWith underlying error`` () = Validation.defaultSuccessWith (fun () -> 1) (Error ["myError"]) =! Error ["myError"]
+
+[<Test>]
+let ``defaultErrorWith underlying ok`` () = Validation.defaultErrorWith (fun () -> ["myError2"]) (Ok 2) =! Ok 2
+
+[<Test>]
+let ``defaultErrorWith underlying empty`` () = Validation.defaultErrorWith (fun () -> ["myError2"]) (Error []) =! Error ["myError2"]
+
+[<Test>]
+let ``defaultErrorWith underlying error`` () = Validation.defaultErrorWith (fun () -> ["myError2"]) (Error ["myError"]) =! Error ["myError"]
